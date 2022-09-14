ff0fb5d16a824eefa17d355b134b25826083d96d
pes <qkrdmstn611@naver.com>
2022-09-14 15:17:47 +0900

    검색 기능 추가

--- main.c
+++ main.c
@@ -24,7 +24,21 @@ void InsertContact(){
     idx++;
     printf("연락처 등록 완료\n");
 }
-
+void SearchContact(){
+    char search[30];
+    int flag = 0;
+    printf("검색할 이름을 입력하세요 : ");
+    scanf(" %s", search );
+    for(int i=0; i<idx;i++){
+        if(strcmp(search, arr[i].name) == 0){
+            printf("이름 : %s\n" ,arr[i].name);
+            printf("연락처 : %s\n" ,arr[i].tel);
+            flag = 1;
+        }
+    }
+    if(flag == 1) printf("연락처 검색 완료\n");
+    else printf("검색한 사용자를 찾을 수 없습니다\n");
+}
 
 enum{
     insert = 1, search, delete, update
@@ -40,7 +54,9 @@ void Controller(){
         case insert:
             InsertContact();
             break;
-        
+        case search:
+            SearchContact();
+            break;
         default:
             printf("프로그램 종료\n");
             break;
